6b57f3afaa850a83e46201c03ce75fa834b9e051
Liz Rurangwa <107972391+ruraliz@users.noreply.github.com>
2023-10-25 23:39:30 -0700

    update updatecourse

--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import ErrorsDisplay from "./ErrorsDisplay";
 // import axios from "axios";
